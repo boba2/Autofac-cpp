ca81aa9e97de664d77411f9aafd6e60a9438b3d6
Bartosz Borowik <bartosz@borowik.net>
2015-10-10 16:23:39 +0200

    Added initial project files

---
+++ DITest/BaseContainerTest.cpp
@@ -0,0 +1,13 @@
+#include "stdafx.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+TEST_CLASS(BaseContainerTest)
+{
+public:
+	TEST_METHOD(TestMethod1)
+	{
+		// TODO: Your test code here
+	}
+};
