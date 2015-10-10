348eaa68197aa5a8d5d53c9f291cbc3640b5fe1e
Bartosz Borowik <bartosz@borowik.net>
2015-10-10 20:47:55 +0200

    Container builder returns the container wrapped in a unique_ptr

--- DITest/BaseContainerTest.cpp
+++ DITest/BaseContainerTest.cpp
@@ -6,10 +6,12 @@
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
+using ServiceInstanceMap = std::unordered_map<std::type_index, void *>;
+
 class Container
 {
 public:
-	explicit Container(std::unordered_map<std::type_index, void *> service_instances)
+	explicit Container(const ServiceInstanceMap service_instances)
 		: _service_instances(service_instances)
 	{}
 
@@ -20,7 +22,7 @@ public:
 	}
 
 private:
-	std::unordered_map<std::type_index, void *> _service_instances;
+	ServiceInstanceMap _service_instances;
 };
 
 class ContainerBuilder
@@ -32,13 +34,13 @@ public:
 		_service_instances[std::type_index(typeid(T))] = service_instance;
 	}
 
-	Container build() const
+	std::unique_ptr<Container> build() const
 	{
-		return Container(_service_instances);
+		return std::make_unique<Container>(_service_instances);
 	}
 
 private:
-	std::unordered_map<std::type_index, void *> _service_instances;
+	ServiceInstanceMap _service_instances;
 };
 
 class Class1 {};
@@ -49,35 +51,36 @@ TEST_CLASS(BaseContainerTest)
 public:
 	TEST_METHOD(ShouldResolveServiceByType_WhenServiceRegistered)
 	{
-		ContainerBuilder container_builder;
+		auto service = std::make_unique<Class1>();
 
-		auto given_service = std::make_unique<Class1>();
+		builder()->registerInstance(service.get());
 
-		container_builder.registerInstance(given_service.get());
-
-		auto container = container_builder.build();
-
-		auto resolved_service = container.resolve<Class1>();
-
-		Assert::IsTrue(resolved_service == given_service.get());
+		Assert::IsTrue(container()->resolve<Class1>() == service.get());
 	}
 
 	TEST_METHOD(ShouldResolveServiceOfCorrectType_WhenServicesOfDifferentTypesRegistered)
 	{
-		ContainerBuilder container_builder;
+		auto service1 = std::make_unique<Class1>();
+		auto service2 = std::make_unique<Class2>();
 
-		auto given_service1 = std::make_unique<Class1>();
-		auto given_service2 = std::make_unique<Class2>();
+		builder()->registerInstance(service1.get());
+		builder()->registerInstance(service2.get());
 
-		container_builder.registerInstance(given_service1.get());
-		container_builder.registerInstance(given_service2.get());
-
-		auto container = container_builder.build();
-
-		auto resolved_service1 = container.resolve<Class1>();
-		auto resolved_service2 = container.resolve<Class2>();
-
-		Assert::IsTrue(resolved_service1 == given_service1.get());
-		Assert::IsTrue(resolved_service2 == given_service2.get());
+		Assert::IsTrue(container()->resolve<Class1>() == service1.get());
+		Assert::IsTrue(container()->resolve<Class2>() == service2.get());
 	}
+
+private:
+	ContainerBuilder *builder() const
+	{
+		return _container_builder.get();
+	}
+
+	Container *container()
+	{
+		return (_container ? _container : _container = builder()->build()).get();
+	}
+
+	std::unique_ptr<ContainerBuilder> _container_builder = std::make_unique<ContainerBuilder>();
+	std::unique_ptr<Container> _container;
 };
