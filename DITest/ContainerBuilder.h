6361730639a9a6059286ae34209b4ce6d8f621ce
Bartosz Borowik <bartosz@borowik.net>
2015-10-18 10:51:09 +0200

    Removed separate ServiceInstances class

--- DITest/ContainerBuilder.h
+++ DITest/ContainerBuilder.h
@@ -1,7 +1,9 @@
 #pragma once
 
-#include "ServiceInstances.h"
 #include "Container.h"
+#include "ServiceInstanceRegisterer.h"
+#include <algorithm>
+#include <iterator>
 
 class ContainerBuilder
 {
@@ -9,14 +11,29 @@ public:
 	template<class T>
 	void registerInstance(T &&instance)
 	{
-		_service_instances.add(std::forward<T>(instance));
+		_service_registerers.insert(std::make_shared<ServiceInstanceRegisterer<typename UnderlyingType<T>::Type>>(std::forward<T>(instance)));
 	}
 
 	std::unique_ptr<Container> build() const
 	{
-		return std::make_unique<Container>(ServiceResolvers(_service_instances.getServiceResolvers()));
+		return std::make_unique<Container>(ServiceResolvers(getServiceResolvers()));
 	}
 
 private:
-	ServiceInstances _service_instances;
+	std::set<std::shared_ptr<ServiceResolver<>>> getServiceResolvers() const
+	{
+		std::set<std::shared_ptr<ServiceResolver<>>> result;
+
+		std::transform(
+			begin(_service_registerers), 
+			end(_service_registerers), 
+			inserter(result, begin(result)), 
+			[](auto registerer) { return registerer->getServiceResolver(); }
+		);
+
+		return result;
+	}
+
+private:
+	std::set<std::shared_ptr<ServiceRegisterer<>>> _service_registerers;
 };
