f7af753ff9874340562efb29efc471cd6d72f895
Bartosz Borowik <bartosz@borowik.net>
2015-10-22 01:26:33 +0200

    Removed unused code from the main function

--- DIGTest/DIGTest.cpp
+++ DIGTest/DIGTest.cpp
@@ -16,18 +16,6 @@ struct AB
 
 int main(int argc, char **argv)
 {
-	auto builder = DI::ContainerBuilder();
-
-	builder.registerInstance(AB());
-//	builder.registerType<AB>();
-
-	std::cout << "-----------------" << std::endl;
-
-	auto ab = builder.build().resolve<AB>();
-	ab.out();
-
-	std::cout << "-----------------" << std::endl;
-
 	::testing::InitGoogleTest(&argc, argv);
 
 	return RUN_ALL_TESTS();
