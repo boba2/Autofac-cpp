45bec29e8197be582eca1da3f115acd598d24557
Bartosz Borowik <bartosz@borowik.net>
2015-10-22 01:25:51 +0200

    Added remaining tests for registering service factory as std::function and lambda with capture

--- DIGTest/ResolveServiceFromFactoryTest.cpp
+++ DIGTest/ResolveServiceFromFactoryTest.cpp
@@ -15,10 +15,51 @@ TEST_F(ResolveServiceFromFactoryTest, ShouldResolveServiceBySharedPtr_WhenServic
 	ASSERT_TRUE(std::dynamic_pointer_cast<DummyService>(container().resolve<std::shared_ptr<DummyService>>()) != nullptr);
 }
 
+TEST_F(ResolveServiceFromFactoryTest, ShouldResolveServiceBySharedPtr_WhenServiceRegisteredAsSharedPtrFactoryFunction)
+{
+	std::function<std::shared_ptr<DummyService>()> factory = [] { return std::make_shared<DummyService>(); };
+
+	builder()
+		.registerFactory(factory);
+
+	ASSERT_TRUE(std::dynamic_pointer_cast<DummyService>(container().resolve<std::shared_ptr<DummyService>>()) != nullptr);
+}
+
+TEST_F(ResolveServiceFromFactoryTest, ShouldResolveServiceBySharedPtr_WhenServiceRegisteredAsSharedPtrFactoryLambdaWithCapture)
+{
+	auto service = std::make_shared<DummyService>();
+
+	builder()
+		.registerFactory([service] { return service; });
+
+	ASSERT_TRUE(std::dynamic_pointer_cast<DummyService>(container().resolve<std::shared_ptr<DummyService>>()) != nullptr);
+}
+
 TEST_F(ResolveServiceFromFactoryTest, ShouldResolveServiceBySharedPtr_WhenServiceRegisteredAsPtrFactory)
 {
 	builder()
-		.registerFactory([] { return new DummyService; });
+		.registerFactory([] { static DummyService service;  return &service; });
+
+	ASSERT_TRUE(std::dynamic_pointer_cast<DummyService>(container().resolve<std::shared_ptr<DummyService>>()) != nullptr);
+}
+
+TEST_F(ResolveServiceFromFactoryTest, ShouldResolveServiceBySharedPtr_WhenServiceRegisteredAsPtrFactoryFunction)
+{
+	std::function<DummyService*()> factory = [] { static DummyService service; return &service; };
+
+	builder()
+		.registerFactory(factory);
+
+	ASSERT_TRUE(std::dynamic_pointer_cast<DummyService>(container().resolve<std::shared_ptr<DummyService>>()) != nullptr);
+}
+
+TEST_F(ResolveServiceFromFactoryTest, ShouldResolveServiceBySharedPtr_WhenServiceRegisteredAsPtrFactoryLambdaWithCapture)
+{
+	auto service = std::make_shared<DummyService>();
+	auto service_ptr = service.get();
+
+	builder()
+		.registerFactory([service_ptr] { return service_ptr; });
 
 	ASSERT_TRUE(std::dynamic_pointer_cast<DummyService>(container().resolve<std::shared_ptr<DummyService>>()) != nullptr);
 }
@@ -27,6 +68,26 @@ TEST_F(ResolveServiceFromFactoryTest, ShouldResolveServiceBySharedPtr_WhenServic
 {
 	builder()
 		.registerFactory([] { return std::make_unique<DummyService>(); });
+
+	ASSERT_TRUE(std::dynamic_pointer_cast<DummyService>(container().resolve<std::shared_ptr<DummyService>>()) != nullptr);
+}
+
+TEST_F(ResolveServiceFromFactoryTest, ShouldResolveServiceBySharedPtr_WhenServiceRegisteredAsUniquePtrFactoryFunction)
+{
+	std::function<std::unique_ptr<DummyService>()> factory = [] { return std::make_unique<DummyService>(); };
+
+	builder()
+		.registerFactory(factory);
+
+	ASSERT_TRUE(std::dynamic_pointer_cast<DummyService>(container().resolve<std::shared_ptr<DummyService>>()) != nullptr);
+}
+
+TEST_F(ResolveServiceFromFactoryTest, ShouldResolveServiceBySharedPtr_WhenServiceRegisteredAsUniquePtrFactoryLambdaWithCapture)
+{
+	auto service = std::make_unique<DummyService>();
+
+	builder()
+		.registerFactory([&service] { return std::move(service); });
 
 	ASSERT_TRUE(std::dynamic_pointer_cast<DummyService>(container().resolve<std::shared_ptr<DummyService>>()) != nullptr);
 }
@@ -45,6 +106,16 @@ TEST_F(ResolveServiceFromFactoryTest, ShouldResolveServiceBySharedPtr_WhenServic
 
 	builder()
 		.registerFactory(factory);
+
+	ASSERT_TRUE(std::dynamic_pointer_cast<DummyService>(container().resolve<std::shared_ptr<DummyService>>()) != nullptr);
+}
+
+TEST_F(ResolveServiceFromFactoryTest, ShouldResolveServiceBySharedPtr_WhenServiceRegisteredAsInstanceFactoryLambdaWithCapture)
+{
+	auto service = DummyService();
+
+	builder()
+		.registerFactory([service] { return service; });
 
 	ASSERT_TRUE(std::dynamic_pointer_cast<DummyService>(container().resolve<std::shared_ptr<DummyService>>()) != nullptr);
 }
